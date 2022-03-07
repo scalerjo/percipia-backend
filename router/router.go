1e2489fca4a411393a7219e72c2015e4c9c9e78d
scalerjo <joseph.scalera1@gmail.com>
2022-03-07 13:23:51 +0000

    change routes to /api/*

--- router/router.go
+++ router/router.go
@@ -14,10 +14,10 @@ func Start() {
 	router := gin.Default()
 
 	// Bind requested resources to internal routes
-	router.GET("/todo", routes.GetAllTODO)
-	router.POST("/todo", routes.CreateTODO)
-	router.PUT("/todo", routes.UpdateTODO)
-	router.DELETE("/todo/:id", routes.DeleteTODO)
+	router.GET("/api/todo", routes.GetAllTODO)
+	router.POST("/api/todo", routes.CreateTODO)
+	router.PUT("/api/todo", routes.UpdateTODO)
+	router.DELETE("/api/todo/:id", routes.DeleteTODO)
 
 	// Start the router on port 8888
 	router.Run("0.0.0.0:8888")
